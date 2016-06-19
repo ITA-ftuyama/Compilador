b56c082b0d2b7e284e819e944f19ac8504b56c23
ftuyama <ftuyama@gmail.com>
2016-06-19 20:43:31 -0300

    Final

--- _Compile.bat
+++ _Compile.bat
@@ -5,7 +5,6 @@ echo #   para Linguagem COMP-ITA 2016     #
 echo ######################################
 
 cd ..
-:loop
 
 echo Chamando Flex
 flex Compilador/_Lab05.l
@@ -20,38 +19,19 @@ gcc y.tab.c main.c yyerror.c -o _Lab05 -lfl
 echo.
 echo Compilando programa
 
-_Lab05  < Compilador/_Lab05Proga.dat > Compilador/_Lab05ProgOutput.dat
+_Lab05  < Compilador/_Lab05Prog.dat > Compilador/_Lab05ProgOutput.dat
 _Lab05  < Compilador/_Lab05Teste.dat > Compilador/_Lab05TesteOutput.dat
 
- goto Teste
-	echo ### Testes - Declaracao de Variaveis
+ goto Testes_Semanticos
 	_Lab05 < Compilador/Bench/BenchDeclaracao.dat > Compilador/Bench/oBenchDeclaracao.dat
-
-	echo ### Testes - Funcao Principal
 	_Lab05 < Compilador/Bench/BenchPrincipal.dat > Compilador/Bench/oBenchPrincipal.dat
-
-	REM echo ### Testes - Variaveis Indexadas 
-	REM _Lab05 < Compilador/Bench/BenchIndexada.dat > Compilador/Bench/oBenchIndexada.dat
-
-	echo ### Testes - Compatibilidade
+	_Lab05 < Compilador/Bench/BenchIndexada.dat > Compilador/Bench/oBenchIndexada.dat
 	_Lab05 < Compilador/Bench/BenchCompatibilidade.dat > Compilador/Bench/oBenchCompatibilidade.dat
-
-	echo ### Testes - Comando Para
 	_Lab05 < Compilador/Bench/BenchPara.dat > Compilador/Bench/oBenchPara.dat
-
-	echo ### Testes - CallFuncao
 	_Lab05 < Compilador/Bench/BenchCallFunc.dat > Compilador/Bench/oBenchCallFunc.dat
-
-	echo ### Testes - RetornoFuncao
 	_Lab05 < Compilador/Bench/BenchRetornoFunc.dat > Compilador/Bench/oBenchRetornoFunc.dat
-
-:Teste
+:Testes_Semanticos
 
 echo Completo
 cd Compilador
 pause
-
-REM Loop compilation with delay
-REM timeout /t 10
-REM goto loop
-
